e8128ae81337e0d39f3b865ea79346319f42a8e5
N4kka <sarlinicola7@gmail.com>
2022-04-11 16:00:59 +0200

    defined Fizz, Buzz and FizzBuzz with different box colours and text

--- Milestone 3/js/script.js
+++ Milestone 3/js/script.js
@@ -1,38 +1,23 @@
-//Printing on console numbers from 1 to 100, call it "Fizz"
- //if it is multiple of 3, "Buzz" if it is multiple of 5
- for (let i = 1; i <= 100; i++) {
-    // 1. Define if it's Fizz or Buzz
-    let fizzOrBuzz;
-
-    if (i % 15 === 0) {
-        fizzOrBuzz = "FizzBuzz";
-    } else if (i % 5 === 0) {
-        fizzOrBuzz = "Buzz";
-    } else if (i % 3 === 0) {
-        fizzOrBuzz = "Fizz";
-    }
-
-    
-    console.log(i, fizzOrBuzz);
-}
-
-//Print on HTML page
-// 2. Print the algorithm
-const rowWrapper = document.querySelector('.row');
+// 3. Output
+//Define Fizz, Buzz & FizzBuzz on HTML page with different colours
+const rowWrapper = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
-    rowWrapper.innerHTML += `<div class="box">${i}</div>`
-    console.log(rowWrapper);
+    //Define Fizz or Buzz
     let fizzOrBuzz;
-
+    
     if (i % 15 === 0) {
-        fizzOrBuzz = "FizzBuzz";
+        fizzOrBuzz = "fizzbuzz";
     } else if (i % 5 === 0) {
-        fizzOrBuzz = "Buzz";
+        fizzOrBuzz = "buzz";
     } else if (i % 3 === 0) {
-        fizzOrBuzz = "Fizz";
+        fizzOrBuzz = "fizz";
+    } else {
+        fizzOrBuzz = i;
     }
 
+    // Output on HTML
+    rowWrapper.innerHTML += `<div class="box ${fizzOrBuzz}">${i, fizzOrBuzz}</div>`;
     
     console.log(i, fizzOrBuzz);
 }
